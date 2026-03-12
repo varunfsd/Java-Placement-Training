06de156f3e5bade29df5fdf44a8669711ca1287d
Varun Marineni <varunmarineni005@gmail.com>
2026-03-12 08:47:22 +0530

    DSA: add inverted right angled triangle pattern

---
+++ Training/InvertedRightAngledTrianglePattern.java
@@ -0,0 +1,18 @@
+package training;
+
+import java.util.*;
+
+public class InvertedRightAngledTrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Number:-");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
