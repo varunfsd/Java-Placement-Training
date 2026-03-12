c62fb0f2c76c6fffb9fdf1c516dc04d955f31a87
Varun Marineni <varunmarineni005@gmail.com>
2026-03-12 09:22:18 +0530

    DSA: add diamond pattern in Java

---
+++ Training/DiamondPattern.java
@@ -0,0 +1,30 @@
+package training;
+
+import java.util.*;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Number:-");
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (i * 2) + 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i > 0; i--) {
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
