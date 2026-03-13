b967d31dd672602a61b1f95d13379de9643bb8a5
Varun Marineni <varunmarineni005@gmail.com>
2026-03-13 14:39:56 +0530

    dsa: implement matrix transpose by swapping rows and columns

---
+++ Training/TransposeOfMatrix.java
@@ -0,0 +1,19 @@
+package training;
+
+public class TransposeOfMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                matrix[i][j] = matrix[j][i];
+            }
+        }
+        for (int[] row : matrix) {
+            for (int a : row) {
+                System.out.print(a + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
