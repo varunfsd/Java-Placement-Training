325f50c79be12c580eb84177914160505cc8ebc1
Varun Marineni <varunmarineni005@gmail.com>
2026-03-13 09:37:34 +0530

    dsa: remove unwanted statements from matrix diagonal printing logic

--- Training/PrintDiagonalMatrix.java
+++ Training/PrintDiagonalMatrix.java
@@ -1,7 +1,5 @@
 package training;
 
-import java.util.*;
-
 public class PrintDiagonalMatrix {
     public static void main(String[] args) {
         int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
