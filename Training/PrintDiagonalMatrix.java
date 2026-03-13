68f120865720221b375314e706619925c0cdab49
Varun Marineni <varunmarineni005@gmail.com>
2026-03-13 09:33:57 +0530

    dsa: print primary diagonal elements of matrix

---
+++ Training/PrintDiagonalMatrix.java
@@ -0,0 +1,12 @@
+package training;
+
+import java.util.*;
+
+public class PrintDiagonalMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        for (int i = 0; i < matrix.length; i++) {
+            System.out.println(matrix[i][i]);
+        }
+    }
+}
