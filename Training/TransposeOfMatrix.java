6b73ac8a18f1ab8524d5e98f2f8ea8658ef4deb6
Varun Marineni <varunmarineni005@gmail.com>
2026-03-13 10:50:16 +0530

    dsa: implement transpose of matrix

---
+++ Training/TransposeOfMatrix.java
@@ -0,0 +1,13 @@
+package training;
+
+public class TransposeOfMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.print(matrix[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
