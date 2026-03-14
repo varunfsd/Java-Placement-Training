8fad36f0fd29e7615832dee5534587c74a709924
Varun Marineni <varunmarineni005@gmail.com>
2026-03-14 09:43:51 +0530

    DSA: Implement string palindrome check using two-pointer technique

---
+++ Training/StringPalindrome.java
@@ -0,0 +1,28 @@
+package training;
+
+import java.util.*;
+
+public class StringPalindrome {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The String:-");
+        String str = sc.nextLine();
+        str = str.trim().toLowerCase();
+        int leftPointer = 0, rightPointer = str.length() - 1;
+        boolean isPalindrome = true;
+        while (leftPointer < rightPointer) {
+            if (str.charAt(leftPointer) != str.charAt(rightPointer)) {
+                isPalindrome = false;
+                break;
+            }
+            leftPointer++;
+            rightPointer--;
+        }
+        if (isPalindrome)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not a Palindrome");
+        sc.close();
+        ;
+    }
+}
