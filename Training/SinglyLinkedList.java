bba271e938e0e8357c673291700303b568ecfc33
Varun Marineni <varunmarineni005@gmail.com>
2026-03-24 14:35:54 +0530

    Enhanced singly linked list implementation by adding insertion, deletion.

--- Training/SinglyLinkedList.java
+++ Training/SinglyLinkedList.java
@@ -12,9 +12,11 @@ class Node {
 
 class LinkedList {
     Node head, tail;
+    int size = 0;
 
     void addNode(int data) {
         Node newnode = new Node(data);
+        size++;
         if (head == null) {
             head = newnode;
             tail = newnode;
@@ -24,23 +26,140 @@ class LinkedList {
         tail = newnode;
     }
 
+    void addFirst(int data) {
+        Node newnode = new Node(data);
+        size++;
+        if (head == null) {
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+
+        newnode.next = head;
+        head = newnode;
+    }
+
+    void addLast(int data) {
+        Node newnode = new Node(data);
+        size++;
+        if (head == null) {
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+
+        tail.next = newnode;
+        tail = newnode;
+    }
+
+    void addMid(int data, int pos) {
+        if (pos <= 0 || pos > size + 1) {
+            System.out.println("Invalid position");
+            return;
+        }
+
+        if (pos == 1) {
+            addFirst(data);
+            return;
+        }
+
+        if (pos == size + 1) {
+            addLast(data);
+            return;
+        }
+
+        Node newnode = new Node(data);
+        Node temp = head;
+
+        for (int i = 1; i < pos - 1; i++) {
+            temp = temp.next;
+        }
+
+        newnode.next = temp.next;
+        temp.next = newnode;
+        size++;
+    }
+
+    void delFirst() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        head = head.next;
+        size--;
+
+        if (head == null) { // list became empty
+            tail = null;
+        }
+    }
+
+    void delLast() {
+        if (head == null) {
+            System.out.println("List is empty");
+            return;
+        }
+
+        if (head == tail) { // only one node
+            head = null;
+            tail = null;
+            size--;
+            return;
+        }
+
+        Node temp = head;
+
+        while (temp.next != tail) {
+            temp = temp.next;
+        }
+
+        temp.next = null;
+        tail = temp; // correct
+        size--;
+    }
+
+    void delMid(int pos) {
+        if (pos <= 0 || pos > size) {
+            System.out.println("Invalid position");
+            return;
+        }
+
+        if (pos == 1) {
+            delFirst();
+            return;
+        }
+
+        if (pos == size) {
+            delLast();
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; i < pos - 1; i++) {
+            temp = temp.next;
+        }
+        temp.next = temp.next.next;
+        size--;
+    }
+
     void displayList() {
         Node temp = head;
         while (temp != null) {
             System.out.print(temp.next != null ? temp.data + " -> " : temp.data);
             temp = temp.next;
         }
+        System.out.println();
     }
 }
 
 public class SinglyLinkedList {
     public static void main(String[] args) {
         LinkedList list = new LinkedList();
+        list.addNode(0);
         list.addNode(1);
         list.addNode(2);
         list.addNode(3);
-        list.addNode(4);
-        list.addNode(5);
+        list.delMid(2);
         list.displayList();
+        System.out.println(list.size);
     }
 }
---
+++ jpmt/MinNoCoins.java
@@ -0,0 +1,11 @@
+package jpmt;
+
+import java.util.*;
+
+public class MinNoCoins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter The Sum:-");
+        int sum = sc.nextInt();
+    }
+}
