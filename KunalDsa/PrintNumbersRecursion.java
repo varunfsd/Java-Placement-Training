e2d6812178490e885191a594660dcade17acd0fe
Varun Marineni <varunmarineni005@gmail.com>
2026-03-31 08:11:33 +0530

    Add recursive solution to print numbers

---
+++ KunalDsa/PrintNumbersRecursion.java
@@ -0,0 +1,14 @@
+package kunaldsa;
+
+public class PrintNumbersRecursion {
+    public static void printNumbers(int n) {
+        if (n == 0)
+            return;
+        System.out.println(n);
+        printNumbers(n - 1);
+    }
+
+    public static void main(String[] args) {
+        printNumbers(5);
+    }
+}
