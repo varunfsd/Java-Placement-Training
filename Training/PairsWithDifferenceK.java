df317d37afd5851a539ab21abe95b9035a7c8a8d
Varun Marineni <varunmarineni005@gmail.com>
2026-04-08 10:38:58 +0530

    Add PairsWithDifferenceK Problem

---
+++ Training/PairsWithDifferenceK.java
@@ -0,0 +1,31 @@
+package training;
+
+public class PairsWithDifferenceK {
+    public static void main(String[] args) {
+        int[] arr = { -3, 0, 1, 3, 6, 8, 11, 14, 21, 25 };
+        int left = 0, right = 1;
+        int k = 5;
+        boolean found = false;
+
+        while (right < arr.length && left < arr.length) {
+            if (left == right) {
+                right++;
+                continue;
+            }
+            int diff = arr[right] - arr[left];
+            if (diff == k) {
+                System.out.println(arr[left] + ", " + arr[right]);
+                found = true;
+                break;
+            } else if (diff < k) {
+                right++;
+            } else {
+                left++;
+            }
+        }
+
+        if (!found) {
+            System.out.println("Not Found");
+        }
+    }
+}
